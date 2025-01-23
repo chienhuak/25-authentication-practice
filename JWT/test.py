6bbf860ceaa5c6831a56694fd09248586c14910e
chienhuak <christy.kch.c@gmail.com>
2025-01-23 11:45:34 +0800

    哈希密碼 & 生成 ID

--- JWT/test.py
+++ JWT/test.py
@@ -1,10 +1,12 @@
 from fastapi import *
 from fastapi.responses import JSONResponse
+import bcrypt # 將密碼哈希處理
 import jwt
 
 app=FastAPI(debug=True)
 
 fake_db = []
+current_id = 1
 # register_test_input = {
 # 	"username": "john_doe",
 # 	"password": "StrongP@ssw0rd"
@@ -15,13 +17,23 @@ fake_db = []
 @app.post("/api/users/register", response_class=JSONResponse)
 async def register(request: Request, data:dict):
 	try:
-		fake_db.append(data)
+		hashed_password = hash_password(data["password"])
+
+		global current_id
+		data["id"] = current_id
+
+		fake_db.append({
+			"id": data["id"],
+			"username": data["username"],
+			"password": hashed_password
+		})
+		current_id += 1
 		print(fake_db)
 
 		return JSONResponse(status_code=201, content={
 			"message": "User registered successfully.",
 			"user": {
-				"id": 1,
+				"id": data["id"],
 				"username": data["username"]
 			}
 		})
@@ -31,3 +43,13 @@ async def register(request: Request, data:dict):
 				"error": True,
 				"message": e
 				}) 
+
+
+
+# 函數的內容，對密碼進行哈希處理並返回結果
+def hash_password(password: str) -> str:
+	# 生成鹽，並用bcrypt進行哈希
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password.decode('utf-8')  # 返回加密後的密碼
+    
