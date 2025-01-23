166c3a31b56b2ec6e14b737b3d351f555c6a16df
chienhuak <christy.kch.c@gmail.com>
2025-01-23 18:49:06 +0800

    使用 bcrypt.checkpw 驗證用戶登入的密碼是否正確

--- JWT/test.js
+++ JWT/test.js
@@ -7,8 +7,8 @@ async function register() {
           'Content-Type': 'application/json'
           },
         body: JSON.stringify({ 
-          "username":test_register_input["username"], 
-          "password":test_register_input["password"] 
+          "username":test_input["username"], 
+          "password":test_input["password"] 
           })
       })
 
@@ -26,8 +26,40 @@ async function register() {
 }
 
 
-const test_register_input = {
+// 登入驗證
+async function signin() {
+
+	const response = await fetch('http://127.0.0.1:8000/api/users/signin', {
+		method: 'POST',
+		headers: {
+		  'Content-Type': 'application/json'
+		  },
+		body: JSON.stringify({ 
+		  "username":test_input["username"], 
+		  "password":test_input["password"] 
+		  })
+	  })
+  
+	  if (response.ok) {
+		const result = await response.json()
+		console.log(result) //糾錯
+		if(result.token)
+		  console.log(result.message)
+		else
+		  console.log('登入失敗或未返回 token')
+  
+	  } else {
+		const result = await response.json()
+		console.log(result.message)
+	  }
+  }
+
+
+
+// 測試
+const test_input = {
 	"username": "john_doe",
 	"password": "StrongP@ssw0rd"
 }
 register()
+signin()
--- JWT/test.py
+++ JWT/test.py
@@ -7,10 +7,49 @@ app=FastAPI(debug=True)
 
 fake_db = []
 current_id = 1
-# register_test_input = {
+# { "id": "1",
 # 	"username": "john_doe",
 # 	"password": "StrongP@ssw0rd"
 # }
+
+
+# 登入驗證
+@app.post("/api/users/signin", response_class=JSONResponse)
+async def signin(request: Request, data:dict):
+	try:
+
+		data = await request.json()  # 從 request body 中取得資料
+		print(data)
+
+		username = data["username"]
+		pw = data["password"]
+
+
+		for i in fake_db:
+			if username == i["username"] and bcrypt.checkpw(pw.encode('utf-8'), i["password"].encode('utf-8')):
+
+				print(f"登入成功: {username}")
+
+
+				return JSONResponse(status_code=201, content={
+					"message": "登入成功。",
+					"token": "TBD"
+				})
+			else :
+				print("登入失敗")
+				return JSONResponse(status_code=201, content={
+					"message": "登入失敗。",
+					"token": "TBD"
+				})
+
+	except Exception as e:
+		print("其他錯誤。")
+		return JSONResponse(status_code=500, content={
+				"error": True,
+				"message": str(e)  
+				# 出現 Exception，但前端拿到 null。
+				# 原因：e 是一個例外物件（Exception），它無法直接被 JSON 序列化（即無法轉換為 JSON 格式），因此需將 e 轉換為字串。
+				}) 
 
 
 # 註冊
@@ -31,7 +70,7 @@ async def register(request: Request, data:dict):
 		print(fake_db)
 
 		return JSONResponse(status_code=201, content={
-			"message": "User registered successfully.",
+			"message": "註冊成功。",
 			"user": {
 				"id": data["id"],
 				"username": data["username"]
@@ -46,10 +85,10 @@ async def register(request: Request, data:dict):
 
 
 
-# 函數的內容，對密碼進行哈希處理並返回結果
+# 對密碼進行哈希處理並返回結果的函數
 def hash_password(password: str) -> str:
 	# 生成鹽，並用bcrypt進行哈希
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password.decode('utf-8')  # 返回加密後的密碼
-    
+	salt = bcrypt.gensalt()
+	hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+	return hashed_password.decode('utf-8')  # 返回加密後的密碼
+	
